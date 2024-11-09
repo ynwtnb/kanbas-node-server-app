a571525195c7775074dd3dd0cee67f3650314c55
Yuna Watanabe <yuna-watanabe1923@g.ecc.u-tokyo.ac.jp>
2024-11-08 19:20:38 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import Hello from './Hello.js';
+import Lab5 from './Lab5/index.js';
+import cors from "cors";
+
+const app = express()
+app.use(cors());   
+app.use(express.json());
+Hello(app)
+Lab5(app)
+app.listen(process.env.PORT || 4000)
