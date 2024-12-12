4057c8d734a2df43f156503227727d92032ac9d7
Yuna Watanabe <yuna-watanabe1923@g.ecc.u-tokyo.ac.jp>
2024-12-12 01:56:23 -0500

    updated dao and routes to delete enrollments when deleting a course

--- Kanbas/Courses/routes.js
+++ Kanbas/Courses/routes.js
@@ -18,8 +18,10 @@ export default function CourseRoutes(app) {
 	});
 	app.delete("/api/courses/:courseId", async (req, res) => {
 		const { courseId } = req.params;
-		const status = await dao.deleteCourse(courseId);
-		res.send(status);
+		const status1 = await dao.deleteCourse(courseId);
+		res.send(status1);
+		const status2 = await enrollmentsDao.deleteEnrollmentsForCourse(courseId);
+		res.send(status2);
 	});
 	app.put("/api/courses/:courseId", async (req, res) => {
 		const { courseId } = req.params;
--- Kanbas/Enrollments/dao.js
+++ Kanbas/Enrollments/dao.js
@@ -29,3 +29,6 @@ export async function findCoursesForUser(userId) {
 	const enrollments = await model.find({ user: userId }).populate("course");
 	return enrollments.map((enrollment) => enrollment.course);
 }
+export async function deleteEnrollmentsForCourse(courseId) {
+	return model.deleteMany({ course: courseId });
+}
